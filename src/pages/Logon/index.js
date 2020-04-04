36a801d5cd41bd73df6e63a24f2a56e63cd6e599
Antonio Vinicius Rocha Araujo <anvinicius@engdb.infra>
2020-04-04 15:06:56 -0300

    Enviando o projeto para o repositório

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './styles.css';
+import { Link } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
 
 import LogoImg from '../../Assets/logo.svg';
@@ -16,12 +17,12 @@ export default  function logon() {
                         Faça seu logon
                             </h1>
                     <input placeholder="Sua ID" />
-                    <FiLogIn size={16} color="#E02041"/>
                     <button className="button" type="submit">Entrar</button>
 
-                <a href="/register">
-                    Não tenho cadastro
-                </a>
+                <Link classname="back-link" to="/register">
+                        <FiLogIn size={16} color="#E02041" />
+                        Não tenho cadastro
+                </Link>
                 </form>
             </section>
 
